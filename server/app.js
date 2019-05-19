b35e56d7dcf8cb0fc58e83b3099a77f5c09cf78b
Wentan Bai <wentan1995@gmail.com>
2019-05-19 07:43:46 -0400

    client and serve first release

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const posts = require('./routes/api/posts')
+app.use('/api/posts', posts)
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Sever started on port ${port}`)
+})
